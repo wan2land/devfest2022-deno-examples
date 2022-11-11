551425fe1da28289a70edbd6a5f5ff94055a230f
Changwan Jun <wan2land@gmail.com>
2022-11-12 01:00:33 +0900

    add vercel

---
+++ vercel/api/index.ts
@@ -0,0 +1,10 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+
+export default function (req: VercelRequest, res: VercelResponse) {
+  const { message = null } = req.query;
+  res.setHeader("Content-Type", "application/json");
+  res.send(JSON.stringify({
+    message: message ?? null,
+    version: 'v1',
+  }));
+}
