085f2d53cf9dbbb78f079cda2005f7b2d698046d
Changwan Jun <wan2land@gmail.com>
2022-11-12 01:27:30 +0900

    add deno_deploy

---
+++ deno_deploy/entry.ts
@@ -0,0 +1,9 @@
+import { serve } from "https://deno.land/std@0.155.0/http/server.ts";
+
+serve((req: Request) => {
+  const url = new URL(req.url);
+  return Response.json({
+    message: url.searchParams.get("message") ?? null,
+    version: "v1",
+  });
+});
