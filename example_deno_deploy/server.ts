9b2afed5d91b70cc48cd8bf7e51322b9ff350661
Changwan Jun <wan2land@gmail.com>
2022-11-12 13:23:19 +0900

    add example deno_deploy

---
+++ example_deno_deploy/server.ts
@@ -0,0 +1,29 @@
+import { serve } from "https://deno.land/std@0.163.0/http/server.ts";
+
+const routes = [] as [
+  pattern: URLPattern,
+  handler: (match: URLPatternResult, req: Request) => Response,
+][];
+
+routes.push([
+  new URLPattern({ pathname: "/" }),
+  () => new Response("pong"),
+]);
+
+routes.push([
+  new URLPattern({ pathname: "/books/:id(\\d+)" }),
+  (match) => new Response(`Book: ${match.pathname.groups.id}`),
+]);
+
+function handler(req: Request): Response {
+  for (const [pattern, handler] of routes) {
+    const match = pattern.exec(req.url);
+    if (match) {
+      return handler(match, req);
+    }
+  }
+
+  return new Response("Not Found", { status: 404 });
+}
+
+serve(handler);
