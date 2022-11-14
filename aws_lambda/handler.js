8708a98fd22bfc937d5da95fc792ea2cef757aa1
Changwan Jun <wan2land@gmail.com>
2022-11-15 00:30:33 +0900

    add aws_lambda bench

---
+++ aws_lambda/handler.js
@@ -0,0 +1,14 @@
+"use strict";
+
+module.exports.hello = async (event) => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      message: event.queryStringParameters && event.queryStringParameters.message || null,
+      version: 'v2',
+    }),
+  };
+};
---
+++ aws_lambda_bench.ts
@@ -0,0 +1,42 @@
+const awsUrl =
+  "https://oguarmzimf.execute-api.ap-northeast-2.amazonaws.com/?message=hello";
+
+async function request() {
+  const start = Date.now();
+  const response = await fetch(awsUrl).then((res) => res.json()) as {
+    version: string;
+  };
+  const end = Date.now();
+  return {
+    version: response.version,
+    time: end - start,
+  };
+}
+
+for (;;) {
+  const { version: firstVersion } = await request();
+
+  const start = Date.now();
+  Deno.run({
+    cmd: ["npx", "serverless", "deploy"],
+    cwd: "./aws_lambda",
+    stdout: "null",
+    stderr: "null",
+  }).status().then(() => {
+    console.log(`Command Complete: ${Date.now() - start}ms`);
+  });
+
+  while (true) {
+    const { version, time } = await request();
+    console.log(`Response: ${time}ms`);
+
+    if (version !== firstVersion) {
+      console.log(`Deploy Complete: ${Date.now() - start}ms`);
+      Deno.exit(0);
+    }
+    if (Date.now() - start > 60000) {
+      console.log("Timeout");
+      break;
+    }
+  }
+}
